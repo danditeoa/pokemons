423d44dd03516ec5091d1e909b7c97d06049b993
danditeoa <taci.dandi@gmail.com>
2024-10-18 21:32:04 -0300

    feat: creating card style

--- src/app/components/PokemonCard.js
+++ src/app/components/PokemonCard.js
@@ -1,13 +1,25 @@
 import React from "react";
+import Image from "next/image"; // Importando o componente de imagem otimizado do Next.js
+import styles from './pokemonCard.module.css';
 
-export default function PokemonCard({ name, image, stats }) {
+export default function PokemonCard({ name, image, stats, description }) {
   return (
-    <div style={{ border: '1px solid #ccc', padding: '20px', width: '200px', textAlign: 'center' }}>
-      <h2>{name}</h2>
-      <img src={image} alt={name} style={{ width: '150px', height: '150px' }} />
+    <div className={styles.card}>
+      <h2 className={styles.name}>{name}</h2>
+      <div className={styles.boxImage}>
+        <Image
+          src={image} 
+          alt={name} 
+          width={150} 
+          height={150} 
+          priority={true}
+          className={styles.image}
+        />
+      </div>
       <p><strong>HP:</strong> {stats[0].base_stat}</p>
       <p><strong>Ataque:</strong> {stats[1].base_stat}</p>
       <p><strong>Defesa:</strong> {stats[2].base_stat}</p>
+      <p><strong>Descrição:</strong> {description}</p> {/* Adicionando a descrição */}
     </div>
   );
 }
