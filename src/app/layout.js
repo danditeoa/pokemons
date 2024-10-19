8e81ef6fa79a157724d188c8cdb3da925a594c7f
danditeoa <taci.dandi@gmail.com>
2024-10-18 21:32:22 -0300

    feat: add monteserrat font

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
