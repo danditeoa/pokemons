3fd995ad8106dd287b9e3276ffc620ceeffdb59c
danditeoa <taci.dandi@gmail.com>
2024-10-20 23:31:11 -0300

    feat: add details pokemon style

--- src/app/components/Modal/Modal.js
+++ src/app/components/Modal/Modal.js
@@ -1,6 +1,6 @@
-// Modal.js
 import React from 'react';
-import styles from './modal.module.css'; // Certifique-se de ter um arquivo CSS para o modal
+import styles from './modal.module.css';
+import Image from 'next/image';
 
 const Modal = ({ isOpen, onClose, pokemon }) => {
   if (!isOpen) return null;
@@ -8,12 +8,18 @@ const Modal = ({ isOpen, onClose, pokemon }) => {
   return (
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        <h2>{pokemon.name}</h2>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-        <p><strong>HP:</strong> {pokemon.stats[0]?.base_stat}</p>
-        <p><strong>Ataque:</strong> {pokemon.stats[1]?.base_stat}</p>
-        <p><strong>Defesa:</strong> {pokemon.stats[2]?.base_stat}</p>
-        <p><strong>Descrição:</strong> {pokemon.description || 'Descrição não disponível'}</p>
+        <h2 className={styles.title}>{pokemon.name}</h2>
+
+        <Image
+          src={pokemon.sprites.front_default} alt={pokemon.name}
+          width={150} 
+          height={150} 
+          priority={true}
+        />
+        <p className={styles.stat}><strong>HP:</strong> {pokemon.stats[0]?.base_stat}</p>
+        <p className={styles.stat}><strong>Ataque:</strong> {pokemon.stats[1]?.base_stat}</p>
+        <p className={styles.stat}><strong>Defesa:</strong> {pokemon.stats[2]?.base_stat}</p>
+        {/* <p className={styles.description}><strong>Descrição:</strong> {pokemon.description || 'Descrição não disponível'}</p> */}
         <button className={styles.battleButton} onClick={onClose}>Fechar</button>
       </div>
     </div>
---
+++ src/app/components/__tests__/Modal.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Modal from '../Modal/Modal';
+
+const mockPokemon = {
+  name: 'Pikachu',
+  sprites: {
+    front_default: '/pikachu.png',
+  },
+  stats: [
+    { base_stat: 35 },
+    { base_stat: 55 },
+    { base_stat: 40 },
+  ],
+};
+
+describe('Modal Component', () => {
+  test('should not render when isOpen is false', () => {
+    const { container } = render(<Modal isOpen={false} onClose={() => {}} pokemon={mockPokemon} />);
+    expect(container.firstChild).toBeNull();
+  });
+
+  test('should render correctly when isOpen is true', () => {
+    render(<Modal isOpen={true} onClose={() => {}} pokemon={mockPokemon} />);
+
+    
+    expect(screen.getByText('Pikachu')).toBeInTheDocument();
+
+    expect(screen.getByText(/HP:/)).toBeInTheDocument();
+    expect(screen.getByText(/Ataque:/)).toBeInTheDocument();
+    expect(screen.getByText(/Defesa:/)).toBeInTheDocument();
+    
+    // checaimagem do Pokémon foi renderizada
+    const image = screen.getByAltText('Pikachu');
+    expect(image).toBeInTheDocument();
+
+    expect(image.src).toContain(encodeURIComponent('/pikachu.png'));
+  });
+
+  test('should call onClose when the "Close" button is clicked', () => {
+    const handleClose = jest.fn();
+    render(<Modal isOpen={true} onClose={handleClose} pokemon={mockPokemon} />);
+    
+    fireEvent.click(screen.getByText('Fechar'));
+    
+    expect(handleClose).toHaveBeenCalledTimes(1);
+  });
+
+  test('should prevent closing when clicking inside the modal', () => {
+    const handleClose = jest.fn();
+    render(<Modal isOpen={true} onClose={handleClose} pokemon={mockPokemon} />);
+    
+    fireEvent.click(screen.getByText('Pikachu'));
+    expect(handleClose).not.toHaveBeenCalled();
+  });
+});
